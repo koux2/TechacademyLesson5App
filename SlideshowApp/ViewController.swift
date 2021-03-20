072c5438e30fba70fed914715b13eb0e1c0ed49d
koux2 <koux2.g@gmail.com>
2021-03-21 01:00:30 +0900

    storyboardにパーツを配置するまで実装

--- SlideshowApp/ViewController.swift
+++ SlideshowApp/ViewController.swift
@@ -8,12 +8,32 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    // UIImage のインスタンスを設定
+    let image1 = UIImage(named:"image1")
+    let image2 = UIImage(named:"image2")
+    let image3 = UIImage(named:"image3")
+    let image4 = UIImage(named:"image4")
+    let image5 = UIImage(named:"image5")
+    let image6 = UIImage(named:"image6")
 
+    @IBOutlet weak var image: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        image.setImage(image1, for: .normal)
     }
-
-
+    
+    @IBAction func unwind(_ segue: UIStoryboardSegue) {
+    }
+    
+    @IBAction func back(_ sender: Any) {
+    }
+    
+    @IBAction func foward(_ sender: Any) {
+    }
+    
+    @IBAction func pause(_ sender: Any) {
+    }
 }
-
